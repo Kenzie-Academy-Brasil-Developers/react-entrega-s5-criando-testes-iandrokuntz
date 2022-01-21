c69306954f23cbc674586b39bc859c0ff4b0096d
iandrokuntz <ianbertoluchi99@gmail.com>
2022-01-21 18:29:30 -0300

    atividade atualizada//completa

---
+++ src/__tests__/components/input.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import { Input } from "semantic-ui-react";
+
+describe("Input test", () => {
+
+  it("should be render input", () => {
+
+    render(<Input placeholder="Insira o CEP" />)
+
+    expect(screen.getByPlaceholderText("Insira o CEP")).toBeTruthy()
+
+  })
+})
